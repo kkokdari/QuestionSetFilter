27603ff12c8b23a3c9fd042ae0d320bb3e371309
naxingyu <asr.naxingyu@gmail.com>
2013-07-02 18:30:22 +0200

    bug fixed

--- qstfilter.pl
+++ qstfilter.pl
@@ -2,13 +2,13 @@
 # Author : Xingyu Na
 # Data   : April 2012
 
-die "qsfilter.pl cat.lab question.hed > question.filter.hed" if ($#ARGV<1);
+die "usage:\n\tperl qsfilter.pl qs.hed full_label.list\n\n" if($#ARGV<1);
 
 print STDERR "Load data from file ...\n";
-$lines = <>;
+@lines = <>;
 
 @qslist = grep {/QS \"/} @lines;
-@lab = grep {not /QS \"/ and /\d/} @lines; # "
+@lab = grep {not /QS \"/ and /\d/} @lines; 
 
 keys(%class) = $#qslist;
 $qsnum = $#qslist;
@@ -16,46 +16,23 @@ $used = 0;
 print STDERR "start filtering ...\n";
 for $qsid (0..$qsnum)
 {
-	$qslist[$qsid]=~/\"(.*)\"\s+\{(.*?)\}/;
-	$qstext = $1;
-	$qsanswers = $2;
-	print STDERR "  checking($qsid|$used|$qsnum):  $qsid.$qstext\t\t";
-	if($qstext=~/[\<\>]\=\d+/)
+	$qslist[$qsid]=~/\"(.*)\"/;
+	print STDERR "  checking($qsid|$used|$qsnum):  $qsid.$1\t\t";
+	$qslist[$qsid]=~/\{(.+?)\}/;
+	$buf=$1;
+	$buf=~s/\*//g;
+	@answer=split /,/,$buf;
+	$yes="";
+	$n=0;
+	for $labid (0..$#lab)
 	{
-		die unless $qsanswers=~/^\**(.)(\d+)(.)\**,/;
-		$minnum = $2;
-		die unless $qsanswers=~/^\**(.)(\d+)(.)\**$/;
-		$maxnum = $2;
-		$leftmark = "\\".$1;
-		$rightmark = "\\".$3;
-		$yes = "";
-		$n = 0;
-		for $labid (0..$#lab)
+		for (@answer)
 		{
-			die unless $lab[$labid]=~/$leftmark(x|\d+)$rightmark/;
-			if($1 ne 'x' and $1<=$maxnum and $1>=minnum)
+			if(index($lab[$labid],$_)>=0)
 			{
 				$n++;
-				$yes.=$labid.'n';
-			}
-		}
-	}
-	else
-	{
-		$qsanswers=~s/\*//g;
-		@answer = split /,/, $qsanswers;
-		$yes = "";
-		$n = 0;
-		for $labid (0..$#lab)
-		{
-			for (@answer)
-			{
-				if(index($lab[$labid],$_)>=0)
-				{
-					$m++;
-					$yes.=$labid.'n';
-					last;
-				}
+				$yes.=$labid;
+				last;
 			}
 		}
 	}
